a0f5415504c310905644a774cc358fd7f9250e3a
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-01-24 13:36:17 -0300

    style: enhanced MobileHeaderStyle

--- src/components/Header/MobileHeader/MobileHeaderItem/MobileHeaderItem.tsx
+++ src/components/Header/MobileHeader/MobileHeaderItem/MobileHeaderItem.tsx
@@ -1,8 +1,24 @@
+import { usePathname } from "next/navigation";
 import { IconRetriever } from "@/components/common/IconRetriever/IconRetriever";
 
 export function MobileHeaderItem({ title }: { title: string }) {
+  const pathname = usePathname();
+
+  const logicalController = { displayBottomStyle: "", borderFormat: null };
+  const bottomStyle =
+    "before:absolute before:bottom-2 before:h-1 before:min-w-[3rem] before:rounded-md before:bg-gradient-to-r from-secondary-blue to-secondary-lightBlue";
+
+  if (pathname.length === 1 && title === "About me") {
+    logicalController.displayBottomStyle = bottomStyle;
+  } else if (pathname.toLowerCase() === title) {
+    logicalController.displayBottomStyle = bottomStyle;
+  }
+
   return (
-    <li className="h-full flex flex-col items-center justify-center">
+    <li
+      tabIndex={0}
+      className={`h-full w-full flex flex-col items-center justify-center p-2 rounded-full focus:bg-primary-transparent ${logicalController.displayBottomStyle}`}
+    >
       <IconRetriever
         iconName={title}
         style="[&_path]:stroke-[#ffffff]"
@@ -10,7 +26,7 @@ export function MobileHeaderItem({ title }: { title: string }) {
         height={25}
         pathVariant="headerIcons\"
       />
-      <span className="font-semibold text-md text-white">{title}</span>
+      <span className="font-semibold text-sm text-white">{title}</span>
     </li>
   );
 }
