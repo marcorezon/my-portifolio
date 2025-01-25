fdbe0881c56c0caa64662a84721b09bfc6924c6e
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-01-25 01:55:15 -0300

    refactor: redefined PresentationArticle logic for Project display

--- src/components/Presentation/PresentationArticle/PresentationArticle.tsx
+++ src/components/Presentation/PresentationArticle/PresentationArticle.tsx
@@ -2,8 +2,12 @@ import { Article } from "@/components/common/Article/Article";
 import { ProjectDisplayer } from "@/components/common/ProjectDisplayer.tsx/ProjectDisplayer";
 import applicationData from "../../../data/applicationData.json";
 import styles from "./PresentationArticle.module.css";
+import { ProjectsProps } from "@/shared/interfaces";
 
 export function PresentationArticle() {
+  const lastProjectIndex = applicationData.projects.length - 1;
+  const lastProject = applicationData.projects[lastProjectIndex];
+
   return (
     <section
       className={`w-full flex flex-col items-start gap-[5vh] my-6 ${styles.presentation}`}
@@ -11,9 +15,7 @@ export function PresentationArticle() {
       <h3 className="text-2xl font-bold ml-[15vw]">Last project</h3>
       <div className="w-full max-w-[1043px] self-center flex flex-wrap items-center justify-evenly gap-8">
         <Article />
-        {applicationData.projects.map((project, index) => (
-          <ProjectDisplayer key={index} project={project} />
-        ))}
+        <ProjectDisplayer project={lastProject as ProjectsProps} />
       </div>
     </section>
   );
