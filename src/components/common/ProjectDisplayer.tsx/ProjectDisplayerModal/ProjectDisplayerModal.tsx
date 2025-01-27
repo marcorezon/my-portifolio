3709bf4c2332ce8b5512ed97045c214d67018a33
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-01-27 02:43:36 -0300

    feat: enhanced glass style and applied it in ProjectDisplayerModal

--- src/components/common/ProjectDisplayer.tsx/ProjectDisplayerModal/ProjectDisplayerModal.tsx
+++ src/components/common/ProjectDisplayer.tsx/ProjectDisplayerModal/ProjectDisplayerModal.tsx
@@ -1,5 +1,6 @@
 import { ProjectProps } from "@/shared/interfaces";
 import { BackButton } from "../../BackButton/BackButton";
+import styles from "../../../../styles/globalStyles.module.css";
 
 export function ProjectDisplayerModal({
   project,
@@ -10,8 +11,10 @@ export function ProjectDisplayerModal({
 }) {
   return (
     <div className="fixed top-0 w-[100vw] h-[100vh] flex items-center justify-center">
-      <div className="w-[50vw] h-[50vh] flex flex-col bg-gradient-to-r from-secondary-blue to-secondary-lightBlue gap-8 p-10 rounded-3xl">
-        <BackButton handleClick={handleClick} style="self-end"/>
+      <div
+        className={`w-[50vw] h-[50vh] flex flex-col gap-8 p-10 rounded-3xl ${styles.glass}`}
+      >
+        <BackButton handleClick={handleClick} style="self-end" />
         {Object.entries(project).map(([key, value], index) => (
           <li key={index}>
             {key}: {value}
