f8124a67b24db0acc215ca18685ad9cdddd2fe10
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-01-27 06:58:55 -0300

    feat: enhanced ExperiencesDisplayer Carousel style

--- src/components/Presentation/ExperiencesDisplayer/ExperiencesDisplayer.tsx
+++ src/components/Presentation/ExperiencesDisplayer/ExperiencesDisplayer.tsx
@@ -15,12 +15,12 @@ export function ExperiencesDisplayer() {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 3,
-      slidesToSlide: 3, // optional, default to 1.
+      slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 2, // optional, default to 1.
+      slidesToSlide: 1, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -30,15 +30,17 @@ export function ExperiencesDisplayer() {
   };
 
   return (
-    <section className={`w-[70%] h-[400px] ${styles.presentation}`}>
+    <section className={`w-[70%] md:h-[400px] ${styles.presentation}`}>
       <h2 className="w-fit text-2xl mx-auto mb-8">Experiences</h2>
       <Carousel
         className="w-full"
         swipeable={true}
         draggable={true}
         responsive={responsive}
+        autoPlay={true}
+        infinite={true}
+        autoPlaySpeed={3000}
         ssr={true}
-        infinite={undefined}
         keyBoardControl={true}
         customRightArrow={
           <CarouselArrow onClick={handleClick} variant="right" />
@@ -46,6 +48,7 @@ export function ExperiencesDisplayer() {
         customLeftArrow={<CarouselArrow onClick={handleClick} variant="left" />}
         containerClass="h-full overflow-y-scroll"
         itemClass="px-10"
+        removeArrowOnDeviceType={"mobile"}
       >
         {applicationData.about_me.experiences.map((experience, index) => (
           <ExperienceItem key={index} {...experience} />
