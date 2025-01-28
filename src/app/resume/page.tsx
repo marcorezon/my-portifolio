79df68a0f73337c1781765c1d6d24fcbb6d7290a
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-01-28 11:18:01 -0300

    feat: appliod in Resume and moved ExpreriencesDiplayer to a new folder

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,7 +1,10 @@
+import { ExperiencesDisplayer } from "@/components/Resume/ExperiencesDisplayer/ExperiencesDisplayer";
+
 export default function Resume() {
     return (
       <div>
           <h1>Resume</h1>
+          <ExperiencesDisplayer />
       </div>
     );
   }
