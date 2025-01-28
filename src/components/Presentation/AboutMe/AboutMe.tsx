5f9c7d2f575b2a9dbc6faac33e405ce4aad2b845
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-01-28 17:50:20 -0300

    feat: enhanced AboutMe style

--- src/components/Presentation/AboutMe/AboutMe.tsx
+++ src/components/Presentation/AboutMe/AboutMe.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { ListItem } from "@/components/common/ListItem/ListItem";
+import styles from "../../../styles/globalStyles.module.css";
 import applicationData from "../../../data/applicationData.json";
 
 export function AboutMe() {
   return (
-    <section className="w-full h-screen flex flex-wrap items-center justify-center">
+    <section
+      className={`w-full min-h-screen flex flex-wrap items-center justify-center ${styles.presentation}`}
+    >
       <Image
         src="/images/profile.png"
         alt="Image"
@@ -14,15 +17,13 @@ export function AboutMe() {
         quality={100}
         className="rounded-full"
       />
-      <div>
+      <div className="min-h-full flex flex-col gap-[4vh] px-8 lg:w-1/3">
         <p>
-          <span>{applicationData.about_me.initial_message}</span>
-          <br />
           <span>{applicationData.about_me.text}</span>
         </p>
 
         <div>
-          <p className="text-xl mb-3">Technologies:</p>
+          <p className="text-xl mb-3">My professional skills</p>
           <ul className="flex flex-wrap gap-2">
             {applicationData.about_me.technologies.map(
               (technology: { name: string }, index: number) => (
