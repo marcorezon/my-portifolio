d5159dba76296639c45d612431867907ce969730
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-01-29 12:34:00 -0300

    feat: set the information and layou of the Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,16 @@
+import applicationData from "../../data/applicationData.json";
+
 export default function Contact() {
-    return (
-      <div>
-          <h1>Contact</h1>
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <h1>Contact</h1>
+      <div className="w-fit h-full flex flex-col items-start justify-center">
+        {Object.entries(applicationData.contact).map(([key, value], index) => (
+          <code key={index}>
+            const {key} = "{value}"
+          </code>
+        ))}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
