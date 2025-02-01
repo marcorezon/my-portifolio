a52cac5786bf1031b9728e3b93e21319ad697a21
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-02-01 09:44:32 -0300

    feat: enhanced AboutMe style

--- src/components/About/AboutMe/AboutMe.tsx
+++ src/components/About/AboutMe/AboutMe.tsx
@@ -1,30 +1,16 @@
-import { IconRetriever } from "@/components/common/IconRetriever/IconRetriever";
-import { ListItem } from "@/components/common/ListItem/ListItem";
-import styles from "../../../styles/globalStyles.module.css";
 import applicationData from "../../../data/applicationData.json";
+import { StacksDisplater } from "./StacksDisplayer/StacksDisplayer";
 
 export function AboutMe() {
   return (
-    <section
-      className={`w-full min-h-screen flex flex-col items-center justify-center gap-10 ${styles.presentation}`}
-    >
-      <h2>About Me</h2>
-      <p className="w-1/2 text-lg">
-        <span>{applicationData.about_me.text}</span>
-      </p>
-      <div className="w-full flex items-center justify-around gap-2">
-        <div className="w-full h-fit flex flex-col items-start justify-center gap-5 p-[10%]">
-          <IconRetriever iconName="coding" width={40} height={40} />
-          <p className="text-xl mb-3">Front-end skils</p>
-          <ul className="flex flex-wrap gap-5">
-            {applicationData.about_me.technologies.map(
-              (technology: { name: string }, index: number) => (
-                <ListItem key={index} name={technology.name} />
-              )
-            )}
-          </ul>
-        </div>
+    <section className="w-full flex flex-col items-center justify-center gap-[10vh] p-[10%]">
+      <div className="flex flex-col items-center justify-center">
+        <h2>About Me</h2>
+        <p className="w-1/2 text-lg">
+          <span>{applicationData.about_me.text}</span>
+        </p>
       </div>
+      <StacksDisplater />
     </section>
   );
 }
---
+++ src/components/About/AboutMe/StacksDisplayer/StacksDisplayer.tsx
@@ -0,0 +1,19 @@
+import { ListItem } from "@/components/common/ListItem/ListItem";
+import applicationData from "../../../../data/applicationData.json";
+
+export function StacksDisplater() {
+  return (
+    <div className="w-full h-fit flex items-center justify-center gap-5">
+      <div>
+      <p className="text-xl mb-3">Front-end skils</p>
+      <ul className="flex flex-wrap gap-5">
+        {applicationData.about_me.technologies.map(
+          (technology: { name: string }, index: number) => (
+            <ListItem key={index} name={technology.name} />
+          )
+        )}
+      </ul>
+      </div>
+    </div>
+  );
+}
