434e9164dc5a86fcb5740c2bca90489449f15230
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-02-01 08:40:11 -0300

    feat: created and set the Education component

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,11 +1,13 @@
 import { AboutMe } from "@/components/About/AboutMe/AboutMe";
 import { Experiences } from "@/components/About/Experiences/Experiences";
 import applicationData from "../../data/applicationData.json";
+import { Education } from "@/components/About/Education/Education";
 
 export default function About() {
   return (
     <div className="max-w-[1200px] h-full flex flex-col items-center justify-center bg-primary-transparent m-auto">
       <AboutMe />
+      <Education />
       <Experiences />
       <div>
         <h1>Contact</h1>
--- src/components/About/Education/Education.tsx
+++ src/components/About/Education/Education.tsx
@@ -1,29 +1,31 @@
 import Image from "next/image";
 import applicationData from "../../../data/applicationData.json";
-
-export function Experiences() {
+export function Education() {
   return (
     <section className="w-full flex flex-col items-center justify-center">
-      <h4>Experience</h4>
+      <h2>Education</h2>
       <div className="w-full flex flex-col justify-start gap-7 p-[10vh]">
-        {applicationData.about_me.experiences.map((experience, index) => (
+        {applicationData.about_me.education.map((educationItemm, index) => (
           <div
             key={index}
             className="flex items-center justify-start items-start border-b border-primary-transparent py-3 gap-5"
           >
             <Image
-              src={experience.logo}
-              alt={`${experience.company} logo`}
+              src={educationItemm.logo}
+              alt={`${educationItemm.institution} logo`}
               width={70}
               height={70}
               objectFit="cover"
               className="rounded-lg"
             />
+
             <div className="flex flex-col justify-start">
-              <span>{experience.details.role}</span>
-              <span>{experience.company}</span>
-              <span>{experience.details.period}</span>
-              <span>• {experience.details.type}</span>
+              <span>{educationItemm.institution}</span>
+              <span>{educationItemm.degree}</span>
+              <span>{educationItemm.identification}</span>
+              <span>
+                • {educationItemm["start date"]} - {educationItemm["end date"]}
+              </span>
             </div>
           </div>
         ))}
