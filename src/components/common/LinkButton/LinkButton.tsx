cddd20f8a7ff5df10502693979af5ff604bfcf92
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-07-01 17:15:53 -0300

    style: ajusted PresentationHeader link buttons

--- src/components/common/LinkButton/LinkButton.tsx
+++ src/components/common/LinkButton/LinkButton.tsx
@@ -6,7 +6,6 @@ interface LinkButtonProps {
   text?: string;
   iconName?: string;
   isExternal?: boolean;
-  colorVariant?: "normal" | "inverted";
   style?: string;
 }
 
@@ -15,28 +14,19 @@ export function LinkButton({
   text,
   iconName,
   isExternal,
-  colorVariant = "normal",
   style = "",
 }: LinkButtonProps) {
-  const colorVariantControler = {
-    containerStyle: "border-2 border-primary-white",
-    textStyle: "",
-  };
-
-  if (colorVariant === "inverted") {
-    colorVariantControler.containerStyle = "bg-primary-dark";
-    colorVariantControler.textStyle = "text-primary-dark";
-  }
+  
 
   return (
     <Link
       href={path}
       target={isExternal ? "_blank" : "_self"}
       rel={isExternal ? "noopener noreferrer" : ""}
-      className={`w-fit h-fit flex justify-center items-center cursor-pointer gap-2 p-4 rounded-full hover:scale-105 ${colorVariantControler.containerStyle} ${style}`}
+      className={`w-fit h-fit flex justify-center items-center cursor-pointer gap-2 p-4 rounded-full hover:scale-105 ${style}`}
     >
       {iconName && <IconRetriever iconName={iconName} width={24} height={24} />}
-      {text && <span className={colorVariantControler.textStyle}>{text}</span>}
+      {text && <span>{text}</span>}
     </Link>
   );
 }
