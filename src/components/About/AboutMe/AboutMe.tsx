afc18d5665f9fb5f4e5f31d1ca7815029915b382
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-07-01 18:01:33 -0300

    style: removed unnecessary code related to about-me

--- src/components/About/AboutMe/AboutMe.tsx
+++ src/components/About/AboutMe/AboutMe.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { ContactInfo } from "../ContactInfo/ContactInfo";
 import applicationData from "../../../data/applicationData.json";
 
@@ -7,30 +6,11 @@ export function AboutMe() {
     <section className="w-full flexColCentered gap-[10vh] p-[10%]">
       <h2>About Me</h2>
       <div className="flex flex-wrap items-center justify-center gap-10">
-        <Image
-          src="/images/profile.png"
-          width={200}
-          height={200}
-          alt="Profile picture"
-          objectFit="cover"
-          quality={100}
-          className="rounded-3xl"
-        />
         <p className="w-full lg:w-1/2 text-lg">
           <span>{applicationData.about_me.text}</span>
         </p>
       </div>
-      <div className="w-full flex flex-wrap items-center justify-around gap-10">
         <ContactInfo />
-        <div className="flexColCentered">
-          <span className="text-sm text-primary-grey">
-            {applicationData.about_me.age}
-          </span>
-          <span className="text-sm text-primary-grey">
-            {applicationData.about_me.location}
-          </span>
-        </div>
-      </div>
     </section>
   );
 }
--- src/components/About/ContactInfo/ContactInfo.tsx
+++ src/components/About/ContactInfo/ContactInfo.tsx
@@ -2,14 +2,24 @@ import applicationData from "../../../data/applicationData.json";
 
 export function ContactInfo() {
   return (
-    <div className="w-fit h-full flex flex-col justify-start gap-5">
-      <h3>Contact information</h3>
-      <div className="w-fit h-full flex flex-col items-start justify-start">
-        {Object.entries(applicationData.contact).map(([key, value], index) => (
-          <span key={index} className="text-sm">
-            {key}: {value}
-          </span>
-        ))}
+    <div className="w-full flex justify-around items-start">
+      <div className="w-fit h-full flex flex-col justify-start gap-5">
+        <h3>Contact information</h3>
+        <div className="w-fit h-full flex flex-col items-start justify-start">
+          {Object.entries(applicationData.contact).map(
+            ([key, value], index) => (
+              <span key={index} className="text-sm select-text">
+                {key}: {value}
+              </span>
+            )
+          )}
+        </div>
+      </div>
+      <div className="flexColCentered">
+        <h4>Location</h4>
+        <span className="text-sm text-primary-grey">
+          {applicationData.about_me.location}
+        </span>
       </div>
     </div>
   );
