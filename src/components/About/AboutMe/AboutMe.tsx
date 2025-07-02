3363936cc96b6fa2cb1ff4e1b0b45fbead1ca1ab
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-07-02 12:21:22 -0300

    feat: added resume to AboutMe

--- src/components/About/AboutMe/AboutMe.tsx
+++ src/components/About/AboutMe/AboutMe.tsx
@@ -5,12 +5,21 @@ export function AboutMe() {
   return (
     <section className="w-full flexColCentered gap-[10vh] p-[10%]">
       <h2>About Me</h2>
-      <div className="flex flex-wrap items-center justify-center gap-10">
+      <div className="flex flex-col flex-wrap items-center justify-center gap-10">
         <p className="w-full lg:w-1/2 text-lg">
           <span>{applicationData.about_me.text}</span>
         </p>
+        <a
+          href="/files/resume.pdf"
+          download
+          target="_blank"
+          rel="noopener noreferrer"
+          className="border-b-2"
+        >
+          Resume
+        </a>
       </div>
-        <ContactInfo />
+      <ContactInfo />
     </section>
   );
 }
