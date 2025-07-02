e61fe72d57747404fec327dfffdc690f19c2374b
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-07-02 13:39:54 -0300

    fix: removed unused imports

--- src/components/common/LinkButton/LinkButton.tsx
+++ src/components/common/LinkButton/LinkButton.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { IconRetriever } from "../IconRetriever/IconRetriever";
 
 interface LinkButtonProps {
--- src/components/common/ProjectDisplayer/ProjectDisplayerModal/ProjectDisplayerModal.tsx
+++ src/components/common/ProjectDisplayer/ProjectDisplayerModal/ProjectDisplayerModal.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { ProjectProps } from "@/shared/interfaces";
 import { BackButton } from "../../BackButton/BackButton";
 import { IconRetriever } from "../../IconRetriever/IconRetriever";
