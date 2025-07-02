be6b9b4370f0336a036b9786fc01e49bd55bc756
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-07-02 18:47:56 -0300

    feat: uploaded site logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,7 @@ import { ListHeaderItem } from "./ListHeaderItem/ListHeaderItem";
 import { useScreenSize } from "@/hooks/useScreenSize";
 import applicationData from "../../data/applicationData.json";
 import styles from "../../styles/globalStyles.module.css";
+import Image from "next/image";
 
 export function Header() {
   const screenSize = useScreenSize();
@@ -18,10 +19,12 @@ export function Header() {
     <div
       className={`fixed w-full h-[75px] flex items-center justify-around z-10 ${styles.glass}`}
     >
-      <span className="text-lg flex gap-2 items-center">
-        <IconRetriever iconName="coding" width={24} height={24} />
-        {applicationData.header.name}
-      </span>
+      <Image 
+      src={"/images/headerIcons/marco-logo.svg"} 
+      alt="Logo"
+      width={200}
+      height={200}
+      />
       <nav className="flex flex-row items-center justify-around xs:w-[70%] xl:w-[40%]">
         {applicationData.header.options.map(
           (option: { title: string }, index: number) => (
